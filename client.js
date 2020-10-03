949ff4f37717a82f95224977bf49d04a5e99acff
dwisetyo1987 <72244266+dwisetyo1987@users.noreply.github.com>
2020-10-03 13:41:33 +0700

    Add files via upload

--- client.js
+++ client.js
@@ -24,7 +24,7 @@ async function send() {
 
   // Send Push Notification
   console.log("Sending Push...");
-  await fetch("/subscribe", {
+  await fetch("https://goldenrod-flint-thimbleberry.glitch.me/subscribe", {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
