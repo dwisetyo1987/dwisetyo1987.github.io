a1f59831661d78b8f4d42769f90c9be3c8db3dad
dwisetyo1987 <72244266+dwisetyo1987@users.noreply.github.com>
2020-10-11 13:32:25 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+const grantPermission = () => {
+  if (!('Notification' in window)) {
+    alert('This browser does not support system notifications');
+    return;
+  }
+
+  if (Notification.permission === 'granted') {
+    new Notification('You are already subscribed to web notifications');
+    return;
+  }
+
+  if (
+    Notification.permission !== 'denied' ||
+    Notification.permission === 'default'
+  ) {
+    Notification.requestPermission().then(result => {
+      if (result === 'granted') {
+        const notification = new Notification(
+          'Awesome! You will start receiving notifications shortly'
+        );
+      }
+    });
+  }
+};
+
+const showNotification = data => {
+  const title = `${data.pusher.name} pushed to the ${
+    data.repository.name
+  } repo`;
+
+  new Notification(title);
+};
+
+const pusher = new Pusher('<your app key>', {
+  cluster: '<your app cluster>',
+  encrypted: true,
+});
+
+const channel = pusher.subscribe('github');
+channel.bind('push', data => {
+  showNotification(data.payload);
+});
+
+const subscribe = document.getElementById('subscribe');
+subscribe.addEventListener('click', event => {
+  grantPermission();
+  subscribe.parentNode.removeChild(subscribe);
+});
