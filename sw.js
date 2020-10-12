ec3063180ba8161a802d9c4712b654039b56130b
dwisetyo1987 <72244266+dwisetyo1987@users.noreply.github.com>
2020-10-12 09:33:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,14 +4,14 @@ Pusher.setLogger(function(log){
   console.log(log)
 });
 
-var pusher = new Pusher('1fb94680701ab31a3139', {
+var pusher = new Pusher('7c0c7a4697e657e7a1c0', {
   encrypted: true,
   disableStats: true
 });
 
-var channel = pusher.subscribe('test_channel');
+var channel = pusher.subscribe('my-channel');
 
-channel.bind('my_event', function(data) {
+channel.bind('my-event', function(data) {
   self.registration.showNotification(data.title, {
     icon: 'https://avatars3.githubusercontent.com/u/739550?v=3&s=200',
     body: data.message
