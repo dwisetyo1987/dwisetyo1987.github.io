f5a2716a46c2dfba179bd1650851a56e78059302
dwisetyo1987 <72244266+dwisetyo1987@users.noreply.github.com>
2020-10-15 17:03:29 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+let openUrl = ""
+self.addEventListener("push", e => {
+  const data = e.data.json();
+  const options = {
+	  icon: data.icon,
+	  image: data.image,
+	  body: data.body
+  }
+  openUrl = data.url;
+  const notificationPromise = self.registration.showNotification(data.title, options);
+  e.waitUntil(notificationPromise);
+});
+
+self.addEventListener('notificationclick', e => {
+	e.notification.close();
+	e.waitUntil(
+		clients.openWindow(openUrl)
+	);
+});
