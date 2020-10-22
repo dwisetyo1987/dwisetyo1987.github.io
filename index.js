89bba4e769d6eaa738221597efd35f793fc8055c
dwisetyo1987 <72244266+dwisetyo1987@users.noreply.github.com>
2020-10-22 16:49:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,26 @@
-const publicVapidKey = 'BJ-nbtJ-FYKr_MR2acocIByfAs8RlrGG9bzzSQM_RA1p8aqTTNpiUK81EiNkR4y7QRYwoPUT4NJZa_CJ5OzSGG0';
-
 let url = 'wss://lavender-luck-smelt.glitch.me/ws'
 let socket = new WebSocket(url);
 
-navigator.serviceWorker.register('sw.js').then(function(reg){
-  if(reg.installing) {
-    console.log('Service worker installing');
-    setTimeout(() => {
-      location.reload()
-    }, 500)
-  } else if(reg.active) {
-    console.log('Service worker active');
-  }
-
-  if (!(reg.showNotification)) {
-    throw('Notifications aren\'t supported on service workers.');
-    return;
-  }
-
-  if (Notification.permission === 'denied') {
-    throw('The user has blocked notifications.');
-  }
-});
-
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === 'complete') {
-    document.querySelector("#subscribeButton").addEventListener('click', event => {
-      document.querySelector("#subscribeButton").textContent = "Subscribed";
-      Notification.requestPermission();
-    });
-    
-    document.querySelector("#sendButton").addEventListener('click', event => {
-      sendMessage();
-    });
-    
-    if (Notification.permission === 'granted') {
-      document.querySelector("#subscribeButton").textContent = "Subscribed";
-    };
+    document.querySelector("#imageInput").onchange = readImage
+    document.querySelector("#titleInput").onchange = readTitle
+    document.querySelector("#priceInput").onchange = readPrice
+    document.querySelector("#descriptionInput").onchange = readDescription
+    document.querySelector("#whatsappInput").onchange = readWhatsapp
+    document.querySelector("#updateInput").onclick = updateData
+    document.querySelector("#whatsappOutput").onclick = openChat
   }
 });
 
-function sendMessage() {
-  changeButtonColor()
-  .then(sendSocketMessage)
-  .then(clearMessage)
-  
-  function changeButtonColor() {
-    return new Promise((res) => {
-      document.querySelector("#sendButton").className = "btn btn-light btn-lg btn-block";
-      res(true)
-    })
-  }
-	
-  function sendSocketMessage(val) {
-    if (val === true) {
-      return new Promise((res) => {
-        setTimeout(() => {
-          const message = {
-            title: document.querySelector("#title").value,
-            image: document.querySelector("#image").value,
-            body: document.querySelector("#body").value
-          }
-          document.querySelector("#sendButton").className = "btn btn-primary btn-lg btn-block";
-          socket.send(JSON.stringify({ message }));
-          res(true)
-        }, 200)
-      })
-    }
-  }
-  
-  function clearMessage(val) {
-    if (val === true) {
-      return new Promise((res) => {
-        setTimeout(() => {
-          document.querySelector("#title").value = ""
-          document.querySelector("#image").value = ""
-          document.querySelector("#body").value = ""
-          res(true)
-        }, 200)
-      })
-    }
-  }
-}
 
-// handle incoming messages
+
+socket.onopen = function(e) {};
+
 socket.onmessage = function(e) {
   let res = JSON.parse(e.data)
-  sendPush(res)
-    
-  async function sendPush(data) {
-    let message = {
-      title: data.message.title,
-      image: data.message.image,
-      body: data.message.body
-    };
-    
-    const register = await navigator.serviceWorker.register("/sw.js", {
-      scope: "/"
-    });
-
-    const subscription = await register.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-    });
-
-    await fetch("https://lavender-luck-smelt.glitch.me/push", {
-      method: "POST",
-      body: JSON.stringify({ subscription, message }),
-      headers: {
-        "content-type": "application/json"
-      }
-    })    
-  }
-  
+  let data = res.data
+  updateLocal(data)
 };
 
 socket.onclose = function(e) {
@@ -138,17 +46,116 @@ socket.onclose = function(e) {
   }
 };
 
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
+function readImage(e) {
+  let elm = document.querySelector('#imageInput')
+  document.querySelector('#imageOutput').src = elm.value
+}
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
+function readTitle() {
+  let elm = document.querySelector('#titleInput')
+  document.querySelector('#titleOutput').textContent = elm.value.toUpperCase()
+}
 
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
+function readPrice() {
+  let elm = document.querySelector('#priceInput')
+  let price = parseInt(elm.value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.').slice(0, -3)
+  document.querySelector('#priceOutput').textContent = "Rp" + price
+}
+
+function readDescription() {
+  let elm = document.querySelector('#descriptionInput')
+  let desc = elm.value.replace(new RegExp('\n','g'), '<br />')
+  document.querySelector('#descriptionOutput').innerHTML = desc
+}
+
+function readWhatsapp() {
+  let elm = document.querySelector('#whatsappInput')
+  let num = elm.value.toString()
+  let charOne = num.charAt(0)
+  let charTwo = num.charAt(1)
+  let firstTwo = charOne + charTwo
+  if (firstTwo === "62") {
+    document.querySelector('#whatsappOutput').value = num
+  } else if (firstTwo === "08") {
+    let remZero = num.slice(1)
+    document.querySelector('#whatsappOutput').value = "62" + remZero
   }
-  return outputArray;
-};
+}
+
+function updateData() {
+  let data = {
+    image: document.querySelector('#imageInput').value,
+    title: document.querySelector('#titleInput').value,
+    price: document.querySelector('#priceInput').value,
+    description: document.querySelector('#descriptionInput').value,
+    whatsapp: document.querySelector('#whatsappInput').value
+  }
+  
+  changeButtonColor()
+  .then(sendDatabase)
+  
+  function changeButtonColor() {
+    return new Promise((res) => {
+      document.querySelector("#updateInput").className = "btn btn-light btn-lg btn-block mb-3";
+      res(true)
+    })
+  }
+  
+  function sendDatabase(val) {
+    if (val === true) {
+      return new Promise((res) => {
+        setTimeout(() => {
+          document.querySelector("#updateInput").className = "btn btn-warning btn-lg btn-block mb-3";
+          localStorage.setItem('data', JSON.stringify(data))
+          socket.send(JSON.stringify({ data: data }))
+          res(true)
+        }, 200)
+      })
+    }
+  }
+}
+
+function openChat() {
+  changeButtonColor()
+  .then(sendWhatsapp)
+  
+  function changeButtonColor() {
+    return new Promise((res) => {
+      document.querySelector("#whatsappOutput").className = "btn btn-light btn-lg btn-block mb-3";
+      res(true)
+    })
+  }
+  
+  function sendWhatsapp(val) {
+    if (val === true) {
+      return new Promise((res) => {
+        setTimeout(() => {
+          document.querySelector("#whatsappOutput").className = "btn btn-success btn-lg btn-block mb-3";
+          let waNum = document.querySelector('#whatsappOutput').value
+          let urlChat = `https://api.whatsapp.com/send/?phone=${waNum}&text=saya+tertarik+dengan+penawaran+anda`
+          window.open(urlChat, "_blank")
+          res(true)
+        }, 200)
+      })
+    }
+  }
+}
+
+function updateLocal(data) {
+  document.querySelector('#imageOutput').src = data.image
+  document.querySelector('#titleOutput').textContent = data.title.toUpperCase()
+  let price = parseInt(data.price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.').slice(0, -3)
+  document.querySelector('#priceOutput').textContent = "Rp" + price
+  let desc = data.description.replace(new RegExp('\n','g'), '<br />')
+  document.querySelector('#descriptionOutput').innerHTML = desc
+  let num = data.whatsapp.toString()
+  let charOne = num.charAt(0)
+  let charTwo = num.charAt(1)
+  let firstTwo = charOne + charTwo
+  if (firstTwo === "62") {
+    document.querySelector('#whatsappOutput').value = num
+  } else if (firstTwo === "08") {
+    let remZero = num.slice(1)
+    document.querySelector('#whatsappOutput').value = "62" + remZero
+  }
+}
